57d15df509745da92c41e5c50ebc031f129c9bd5
Nuray <nuraynurzhaskyzy@gmail.com>
2024-09-03 14:05:02 +0500

    fix Leftovers model

--- models/leftovers.js
+++ models/leftovers.js
@@ -1,19 +1,15 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database";
-import Item from "./items";
-import Shop from "./shop";
 
 const Leftovers = sequelize.define('Leftovers', {
-    item_id: {
-        number_on_shelf: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        number_in_order: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        }
-    }
+    number_on_shelf: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    number_in_order: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
 });
 
 export default Leftovers;
